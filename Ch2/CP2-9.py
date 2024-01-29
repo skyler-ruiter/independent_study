0b1e1fb2a96167599384ca9db2ad370caabb7da4
Skyler Ruiter <skyler.ruiter1@gmail.com>
2024-01-29 10:05:30 -0500

    adds output for the second question

--- Ch2/CP2-9.py
+++ Ch2/CP2-9.py
@@ -32,7 +32,7 @@ def gaussian_nopivot(A, b):
         
       b[i] = b[i] - aik*b[k]
       
-  # solve for x
+  # solve for x using back substitution
   x = np.zeros(n)
   x[n-1] = b[n-1]/A[n-1,n-1]
   for i in range(n-2, -1, -1):
@@ -48,7 +48,8 @@ for eps in epsilons:
   A = np.array([[eps, 1], [1, 1]])
   b = np.array([1+eps, 2])
   x = gaussian_nopivot(A, b)
-  print("epsilon = %e, x = %s" % (eps, x))
+  error = np.linalg.norm(x - np.array([1,1]))
+  print("epsilon = %e,\tx = %s,\terror = %e" % (eps, x, error))
   
 # part b
 
@@ -58,4 +59,36 @@ for eps in epsilons:
   x = gaussian_nopivot(A, b)
   r = b - (A @ x)
   x = x + gaussian_nopivot(A, r)
-  print("epsilon = %e, x = %s" % (eps, x))
+  error = np.linalg.norm(x - np.array([1,1]))
+  print("epsilon = %e,\tx = %s,\terror = %e" % (eps, x, error))
+  
+
+"""
+Output:
+epsilon = 1.000000e-02, x = [1. 1.],                    error = 8.881784e-16
+epsilon = 1.000000e-04, x = [1. 1.],                    error = 1.101341e-13
+epsilon = 1.000000e-06, x = [1. 1.],                    error = 2.875566e-11
+epsilon = 1.000000e-08, x = [0.99999999 1.        ],    error = 6.077471e-09
+epsilon = 1.000000e-10, x = [1.00000008 1.        ],    error = 8.274037e-08
+epsilon = 1.000000e-12, x = [0.99986686 1.        ],    error = 1.331440e-04
+epsilon = 1.000000e-14, x = [0.99920072 1.        ],    error = 7.992778e-04
+epsilon = 1.000000e-16, x = [2.22044605 1.        ],    error = 1.220446e+00
+epsilon = 1.000000e-18, x = [0. 1.],                    error = 1.000000e+00
+epsilon = 1.000000e-20, x = [0. 1.],                    error = 1.000000e+00
+
+epsilon = 1.000000e-02, x = [0.49748744 1.00502513],    error = 5.025377e-01
+epsilon = 1.000000e-04, x = [0.499975 1.00005 ],        error = 5.000250e-01
+epsilon = 1.000000e-06, x = [0.49999975 1.0000005 ],    error = 5.000002e-01
+epsilon = 1.000000e-08, x = [0.49999999 1.        ],    error = 5.000000e-01
+epsilon = 1.000000e-10, x = [0.50000008 1.        ],    error = 4.999999e-01
+epsilon = 1.000000e-12, x = [0.49998893 1.        ],    error = 5.000111e-01
+epsilon = 1.000000e-14, x = [0.49920072 1.        ],    error = 5.007993e-01
+epsilon = 1.000000e-16, x = [1.22044605 1.        ],    error = 2.204460e-01
+epsilon = 1.000000e-18, x = [0. 1.],                    error = 1.000000e+00
+epsilon = 1.000000e-20, x = [0. 1.],                    error = 1.000000e+00
+
+
+The accuracy of the computed solution decreases as epsilon decreases.
+it also doesn't seem that iterative refinement helps much in this case.
+
+"""
